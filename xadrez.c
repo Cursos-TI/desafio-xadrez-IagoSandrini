a0b681ae1de3ba2fb13c885988310cffcc51eca6
Iago Sandrini <iagosandrini@gmail.com>
2025-08-13 12:58:44 +0000

    feat/nivel-novato

--- xadrez.c
+++ xadrez.c
@@ -4,29 +4,29 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
-int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+int main()
+{
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    int bispo = 0;
+    int torre = 0;
+    int rainha = 0;
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    do
+    {
+        printf("O Bispo andou %d para direita cima\n", bispo);
+        bispo++;
+    } while (bispo <= 5);
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    while (torre <= 5)
+    {
+        printf("A Torre andou %d para direita\n", torre);
+        torre++;
+    }
 
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
-
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
-
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
+    for (rainha; rainha <= 8; rainha++)
+    {
+        printf("A Rainha andou %d para esquerda\n", rainha);
+    }
 
     return 0;
 }
